b771316c7be095eef878ede015404d46f30d5889
IUT BLAGNAC <noreply@univ-tlse2.fr>
2023-12-15 15:32:25 +0100

    TP2 Fin

---
+++ cucumber_demo/src/test/java/dojo/StepDefinitions.java
@@ -0,0 +1,21 @@
+package dojo;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class StepDefinitions {
+
+    @Given("an example scenario")
+    public void anExampleScenario() {
+    }
+
+    @When("all step definitions are implemented")
+    public void allStepDefinitionsAreImplemented() {
+    }
+
+    @Then("the scenario passes")
+    public void theScenarioPasses() {
+    }
+
+}
